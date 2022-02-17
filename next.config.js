22c95ed98d4963a52e2176d4b1b5688e7cab2d0d
chriswdmr <7249920+chriswdmr@users.noreply.github.com>
2022-02-17 19:50:40 +0100

    Add example

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  async rewrites() {
+    return {
+      afterFiles: [
+        {
+          source: '/legacy-page',
+          destination: '/legacy-page/index.html',
+        },
+      ],
+    }
+  },
 }
